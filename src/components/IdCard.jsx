18b633db51324abfff3da8a0588f34305d188906
Argexitoo <argexitoo@gmail.com>
2022-02-02 19:50:19 +0100

    done

--- src/components/IdCard.jsx
+++ src/components/IdCard.jsx
@@ -3,7 +3,6 @@ function IdCard({lastName, firstName, gender, height, birth, picture}) {
         <div>
         <img src={picture} alt="" />
         <h3>First name: {firstName}</h3>
-       
         <h3>Last name: {lastName}</h3>
         <h3>Gender: {gender}</h3>
         <h3>Height: {height}</h3>
